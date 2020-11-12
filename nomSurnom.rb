8cc377c1b782aa3dd57c289bd37006798f27e4a6
brian <brian.mtr@outlook.fr>
2020-11-12 15:39:47 +0100

    add comments exo 3

--- nomSurnom.rb
+++ nomSurnom.rb
@@ -4,15 +4,15 @@ nom =gets.chomp
 
 puts "Entre ton surnom"
 surnom =gets.chomp
-
+#si nom n'est pas vide et surnom est vide tu affiche Hello "nom saisie"
 if nom != '' && surnom == ''
     puts "Hello #{nom}"
 end
-
+#si nom est vide et surnom n'est pas vide tu affcihe Hello "surnom saisie"
 if nom == '' && surnom != ''
 puts "Hello #{surnom}"
 end
-
+#si nom et surnom ne sont pas vide tu affiche "surnom saisie" la premiere lettre en maj
 if nom != '' && surnom != ''
     puts "Hello #{surnom.capitalize}"
 end
